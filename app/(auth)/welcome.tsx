ee4e181a8f4dcb5467b82c58eb8f899ff9a7bdf9
chakravarti-pranjal <pranjal.chakravarti@corrintech.com>
2025-10-19 16:06:52 +0530

    all setup completed

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Onboarding = () => {
+    return (
+        <SafeAreaView>
+            <Text>Onboarding Screen</Text>
+        </SafeAreaView>
+    );
+}
+
+
+export default Onboarding;
